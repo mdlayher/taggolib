f42d09eaa200ed61a3d9d043e7366228e588e8d5
Matt Layher <mdlayher@gmail.com>
2014-05-14 11:22:59 -0400

    Update tests and test file to include tag and property matching

--- taggolib_test.go
+++ taggolib_test.go
@@ -17,20 +17,22 @@ func TestNew(t *testing.T) {
 
 	// Table of tests
 	var tests = []struct {
-		stream []byte
-		parser Parser
-		err    error
+		stream     []byte
+		parser     Parser
+		err        error
+		tags       []string
+		properties []int
 	}{
-		// Check for FLAC file
-		{flacFile, &FLACParser{}, nil},
+		// Check for FLAC file, with hardcoded expected tags and properties
+		{flacFile, &FLACParser{}, nil, []string{"Artist", "Album", "Title"}, []int{5, 202, 16, 44100}},
 
 		// Check for an unknown format
-		{[]byte("nonsense"), nil, ErrUnknownFormat},
+		{[]byte("nonsense"), nil, ErrUnknownFormat, nil, nil},
 	}
 
 	// Iterate all tests
 	for _, test := range tests {
-		// Generate a io.Reader
+		// Generate a io.ReadSeeker
 		reader := bytes.NewReader(test.stream)
 
 		// Attempt to create a parser for the reader
@@ -45,6 +47,47 @@ func TestNew(t *testing.T) {
 		// Verify that the proper parser type was created
 		if reflect.TypeOf(parser) != reflect.TypeOf(test.parser) {
 			t.Fatalf("unexpected parser type: %v", reflect.TypeOf(parser))
+		}
+
+		// Check for valid tags
+		if test.tags != nil {
+			// Artist
+			if parser.Artist() != test.tags[0] {
+				t.Fatalf("mismatched tag Artist: %v != %v", parser.Artist(), test.tags[0])
+			}
+
+			// Album
+			if parser.Album() != test.tags[1] {
+				t.Fatalf("mismatched tag Album: %v != %v", parser.Album(), test.tags[1])
+			}
+
+			// Title
+			if parser.Title() != test.tags[2] {
+				t.Fatalf("mismatched tag Title: %v != %v", parser.Title(), test.tags[2])
+			}
+		}
+
+		// Check for valid properties
+		if test.properties != nil {
+			// Duration
+			if int(parser.Duration().Seconds()) != test.properties[0] {
+				t.Fatalf("mismatched property Duration: %v != %v", parser.Duration().Seconds(), test.properties[0])
+			}
+
+			// Bitrate
+			if parser.Bitrate() != test.properties[1] {
+				t.Fatalf("mismatched property Bitrate: %v != %v", parser.Bitrate(), test.properties[1])
+			}
+
+			// BitDepth
+			if parser.BitDepth() != test.properties[2] {
+				t.Fatalf("mismatched property BitDepth: %v != %v", parser.BitDepth(), test.properties[2])
+			}
+
+			// SampleRate
+			if parser.SampleRate() != test.properties[3] {
+				t.Fatalf("mismatched property SampleRate: %v != %v", parser.SampleRate(), test.properties[3])
+			}
 		}
 	}
 }
