9a82eaf52b3a5b9047b00660051f1d809315f9d9
Matt Layher <mdlayher@gmail.com>
2014-05-21 11:14:37 -0400

    Add taggo example program to repository and README

--- taggo/taggo.go
+++ taggo/taggo.go
@@ -1,34 +1,3 @@
-taggolib [![Build Status](https://travis-ci.org/mdlayher/taggolib.svg?branch=master)](https://travis-ci.org/mdlayher/taggolib) [![GoDoc](http://godoc.org/github.com/mdlayher/taggolib?status.png)](http://godoc.org/github.com/mdlayher/taggolib)
-========
-
-taggolib is a Go package which provides read-only access to metadata contained in various audio formats.  MIT Licensed.
-
-taggolib is inspired by the [TagLib](http://taglib.github.io/) and [taglib-sharp](https://github.com/mono/taglib-sharp/)
-projects.  Its goal is to provide read-only metadata access to a variety of audio formats in Go, without the need
-to use a TagLib binding.
-
-Currently, taggolib supports the following formats, with some caveats:
-
-- FLAC
-- MP3
-  - ID3v2.4 tags only (ID3v2.3 in the works)
-- OGG
-
-Example
-=======
-
-taggolib has a very simple interface, and many tags can be accessed by simply calling an appropriately-named
-method with no parameters.  Below is an example script called `taggo`, which can also be found in this repository
-at [taggo/taggo.go](https://github.com/mdlayher/taggolib/tree/master/taggo/). `taggo` will perform a recursive
-walk on a specified parameter directory, and print out information about any media files it recognizes.
-
-```
-$ cd taggo
-$ go build
-$ ./taggo /home/matt/Music/
-```
-
-```go
 package main
 
 import (
@@ -90,4 +59,3 @@ func main() {
 		return
 	}
 }
-```
