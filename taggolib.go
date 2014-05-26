4812647ba22b29d231f2c0f3e84f4bf636210f0c
Matt Layher <mdlayher@gmail.com>
2014-05-26 14:08:55 -0400

    Beef up taggolib documentation for GoDoc

--- taggolib.go
+++ taggolib.go
@@ -32,20 +32,23 @@ var (
 	errUnsupportedVersion = errors.New("unsupported version")
 )
 
-// TagError represents an error which occurs during the metadata parsing process
+// TagError represents an error which occurs during the metadata parsing process.  It is used internally to
+// note several types of errors, and may be used to retrieve detailed information regarding an error.
 type TagError struct {
 	Err     error
 	Format  string
 	Details string
 }
 
-// Error returns a detailed description of an error during the the metadata parsing process
+// Error returns a detailed description of an error during the the metadata parsing process, including the
+// internal taggolib error, the detected stream format, and a short description of exactly why the error occurred.
 func (e TagError) Error() string {
 	return fmt.Sprintf("%s - %s: %s", e.Err.Error(), e.Format, e.Details)
 }
 
 // IsInvalidStream is a convenience method which checks if an error is caused by an invalid stream
-// of a known format
+// of a known format.  This may happen if the input stream is corrupt, or if the input stream contains flags which
+// should not be present in a valid input stream.
 func IsInvalidStream(err error) bool {
 	// Attempt to type-assert to TagError
 	tagErr, ok := err.(TagError)
@@ -57,7 +60,9 @@ func IsInvalidStream(err error) bool {
 	return tagErr.Err == errInvalidStream
 }
 
-// IsUnknownFormat is a convenience method which checks if an error is caused by an unknown format
+// IsUnknownFormat is a convenience method which checks if an error is caused by an unknown format.  This may happen
+// if the input stream contains a magic number which taggolib cannot handle, such as an unsupported audio format,
+// or any kind of file which is not an audio file.
 func IsUnknownFormat(err error) bool {
 	// Attempt to type-assert to TagError
 	tagErr, ok := err.(TagError)
@@ -70,7 +75,8 @@ func IsUnknownFormat(err error) bool {
 }
 
 // IsUnsupportedVersion is a convenience method which checks if an error is caused by an unsupported version
-// of a known format
+// of a known format.  This may happen if the input stream is recognized by taggolib, but taggolib does not support
+// parsing a certain version of the metadata, such as ID3v1.
 func IsUnsupportedVersion(err error) bool {
 	// Attempt to type-assert to TagError
 	tagErr, ok := err.(TagError)
@@ -82,28 +88,40 @@ func IsUnsupportedVersion(err error) bool {
 	return tagErr.Err == errUnsupportedVersion
 }
 
-// Parser represents an audio metadata tag parser
+// Parser represents an audio metadata tag parser.  It is the interface which all other parsers implement, and it
+// contains all the standard methods which must be present in an audio parser.
 type Parser interface {
+	// Methods which access the data stored in a typical audio metadata tag
 	Album() string
 	AlbumArtist() string
 	Artist() string
-	BitDepth() int
-	Bitrate() int
-	Channels() int
 	Comment() string
 	Date() string
 	DiscNumber() int
+	Genre() string
+	Title() string
+	TrackNumber() int
+
+	// Tag is a special method which will attempt to retrieve an audio metadata tag with the input name. Tag will
+	// attempt to return a metadata tag's raw contents, or will return an empty string on failure.
+	// Using Tag, the following two calls are functionally equivalent:
+	//   - parser.Artist()
+	//   - parser.Tag("ARTIST")
+	Tag(name string) string
+
+	// Methods which access properties of an audio file, which are typically calculated at runtime
+	BitDepth() int
+	Bitrate() int
+	Channels() int
 	Duration() time.Duration
 	Encoder() string
 	Format() string
-	Genre() string
 	SampleRate() int
-	Tag(name string) string
-	Title() string
-	TrackNumber() int
 }
 
-// New creates a new Parser depending on the magic number detected in the input reader
+// New creates a new audio metadata parser, depending on the magic number detected in the input reader.  If New
+// recognizes the magic number, it will delegate parsing to the appropriate parser.  If it does not recognize the
+// input format, it will return errUnknownFormat, which can be checked using IsUnknownFormat.
 func New(reader io.ReadSeeker) (Parser, error) {
 	// Check for magic numbers
 	magicBuf := make([]byte, 8)
