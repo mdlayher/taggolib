0476696d09d9daf326918582d6bffe418799111a
Matt Layher <mdlayher@gmail.com>
2014-05-27 16:18:35 -0400

    taggo: recursively walk all parameter directories

--- taggo/taggo.go
+++ taggo/taggo.go
@@ -24,53 +24,55 @@ func main() {
 		return
 	}
 
-	// Invoke a recursive file walk
-	err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
-		// Skip directories
-		if info.IsDir() {
+	// Invoke a recursive file walk on all parameter directories
+	for _, arg := range os.Args[1:] {
+		err := filepath.Walk(arg, func(path string, info os.FileInfo, err error) error {
+			// Skip directories
+			if info.IsDir() {
+				return nil
+			}
+
+			// Open file
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			// Load file using taggolib
+			audio, err := taggolib.New(file)
+			if err != nil {
+				// Check for unknown format, which will be skipped
+				if taggolib.IsUnknownFormat(err) {
+					return nil
+				}
+
+				// Check for unsupported version, invalid stream, or EOF, which will be logged and skipped
+				if taggolib.IsUnsupportedVersion(err) || taggolib.IsInvalidStream(err) || err == io.EOF {
+					fmt.Println("taggo:", err, ":", path)
+					return nil
+				}
+
+				return err
+			}
+
+			// Calculate duration in mm:ss format
+			seconds := int(audio.Duration().Seconds())
+			minutes := seconds / 60
+			seconds = seconds - (minutes * 60)
+
+			// Print information about file
+			fmt.Printf("%s - %s - %s [#%d.%02d] [%02d:%02d] [%s/%dkbps/%dbit/%dkHz]\n",
+				audio.Artist(), audio.Album(), audio.Title(), audio.DiscNumber(), audio.TrackNumber(),
+				minutes, seconds, audio.Format(), audio.Bitrate(), audio.BitDepth(), audio.SampleRate()/1000)
+
 			return nil
-		}
+		})
 
-		// Open file
-		file, err := os.Open(path)
+		// Check for fatal walk error
 		if err != nil {
-			return err
+			fmt.Println("taggo: fatal:", err)
+			return
 		}
-		defer file.Close()
-
-		// Load file using taggolib
-		audio, err := taggolib.New(file)
-		if err != nil {
-			// Check for unknown format, which will be skipped
-			if taggolib.IsUnknownFormat(err) {
-				return nil
-			}
-
-			// Check for unsupported version, invalid stream, or EOF, which will be logged and skipped
-			if taggolib.IsUnsupportedVersion(err) || taggolib.IsInvalidStream(err) || err == io.EOF {
-				fmt.Println("taggo:", err, ":", path)
-				return nil
-			}
-
-			return err
-		}
-
-		// Calculate duration in mm:ss format
-		seconds := int(audio.Duration().Seconds())
-		minutes := seconds / 60
-		seconds = seconds - (minutes * 60)
-
-		// Print information about file
-		fmt.Printf("%s - %s - %s [#%d.%02d] [%02d:%02d] [%s/%dkbps/%dbit/%dkHz]\n",
-			audio.Artist(), audio.Album(), audio.Title(), audio.DiscNumber(), audio.TrackNumber(),
-			minutes, seconds, audio.Format(), audio.Bitrate(), audio.BitDepth(), audio.SampleRate()/1000)
-
-		return nil
-	})
-
-	// Check for walk error
-	if err != nil {
-		fmt.Println("taggo:", err)
-		return
 	}
 }
